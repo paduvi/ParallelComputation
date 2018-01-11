0e47d8a38a8a6a58720ab6fa472013a684313381
vietpd <viet3695@gmail.com>
2018-01-12 02:10:14 +0700

    update

--- src/StatisticBoyerMoore.java
+++ src/StatisticBoyerMoore.java
@@ -30,7 +30,7 @@ public class StatisticBoyerMoore {
 		int fraction = 20;
 
 		int np = 16;
-		int pow = 8;
+		int pow = 7;
 
 		int times = 5;
 
@@ -47,13 +47,11 @@ public class StatisticBoyerMoore {
 			normalResult[i - 1] = watch(pr);
 		}
 
-		// StringBuilder builder = new StringBuilder();
-
 		for (int i = 1; i <= np; i++) {
 			for (int j = 1; j <= pow; j++) {
 				int m = (int) Math.pow(10, j);
 				int n = m * fraction;
-				String temp = i + "\t" + m + "\t" + normalResult[j - 1];
+				String temp = i + "\t" + n + "\t" + m + "\t" + normalResult[j - 1];
 				double result = 0;
 				int success = 0;
 				for (int k = 0; k < times; k++) {
@@ -78,7 +76,38 @@ public class StatisticBoyerMoore {
 
 				Files.write(Paths.get("report-boyer.txt"), (temp + "\n").getBytes(), StandardOpenOption.CREATE,
 						StandardOpenOption.APPEND);
-				// builder.append(temp).append("\n");
+			}
+		}
+
+		int m = 10;
+		for (int i = 1; i <= np; i++) {
+			for (int j = 2; j <= pow; j++) {
+				int n = (int) Math.pow(10, j);
+				String temp = i + "\t" + n + "\t" + m + "\t" + normalResult[j - 1];
+				double result = 0;
+				int success = 0;
+				for (int k = 0; k < times; k++) {
+					String command = "mpirun -np " + i + " java MpiBoyerMoore " + n + " " + m;
+					System.out.println("\n=============");
+					System.out.println(command);
+					ProcessBuilder builder = new ProcessBuilder(command.split(" "));
+					builder.redirectErrorStream(true);
+					Process pr = builder.start();
+					double tempResult = watch(pr);
+					if (tempResult != -1) {
+						result += tempResult;
+						success++;
+					}
+				}
+
+				if (success != 0) {
+					temp += "\t" + result / success;
+				} else {
+					temp += "\t-1";
+				}
+
+				Files.write(Paths.get("report-boyer.txt"), (temp + "\n").getBytes(), StandardOpenOption.CREATE,
+						StandardOpenOption.APPEND);
 			}
 		}
 
